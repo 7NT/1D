15caeff7234cf7b799fe60ed2b764a13f4cd02d8
7NT <jun@jbridge.net>
2020-05-20 18:04:24 -0700

    5/20

--- src/api.js
+++ src/api.js
@@ -4,12 +4,19 @@ const io = require('socket.io-client')
 const auth = require('@feathersjs/authentication-client')
 
 // Set up Socket.io client with the socket
-const socket = io('http://localhost:3030', { transports: ['websocket', 'polling'], forceNew: true })
+// const socket = io('http://localhost:3030', { transports: ['websocket', 'polling'], forceNew: true })
 // const socket = io('http://www.jbridge.net:3030', { transports: ['websocket'], forceNew: true })
+const socket = io('http://localhost:3030')
+// const socket = io('http://www.jbridge.net:3030')
 
 const api = feathers()
 
 api.configure(socketio(socket))
+api.configure(socketio(socket, {
+  timeout: 20000,
+  pingInterval: 10000,
+  pingTimeout: 50000
+}))
 api.configure(auth())
 
 const users$ = api.service('/users')
@@ -19,6 +26,11 @@ const tables$ = api.service('/tables')
 const results$ = api.service('/results')
 const tourneys$ = api.service('/tourneys')
 const teams$ = api.service('/teams')
+
+api.io.on('disconnect', (reason) => {
+  // Show offline message
+  console.log('disconnect', reason)
+})
 
 // export default api
 export {

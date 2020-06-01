2f414813437c39461398eb7f4c8aea4fbfd014c8
7NT <jun@jbridge.net>
2020-06-01 08:57:00 -0700

    5-31

--- api/src/hooks/tourneys.ts
+++ api/src/hooks/tourneys.ts
@@ -20,7 +20,7 @@ const onPairs = (): Hook => {
 
     // console.log(context)
     if (state) {
-      context.data.pairs = onState(state, pairs)
+      context.data.pairs = onState(context.app, state, pairs)
     } else if (pairs) {
       context.data.pairs = onPair(pairs)
     }
@@ -41,12 +41,39 @@ function onPair (pairs: any[]) {
   return pairs2
 }
 
-function onState(state: number, pairs: any[]) {
-  const n = pairs.length
-  const pairs1 = []
+function onState (app: any, state: number, pairs: any[]) {
+  const tables$ = app.service('tables')
+  const players$ = app.service('players')
 
+  const pairs1 = shuffle(pairs)
+  const N = Math.floor(pairs1.length / 2)
+  for (let i = 0; i < N; i++) {
+    pairs1[i].state = state
+    pairs1[N + i].state = state
 
+  }
+  return pairs1
 }
+
+function shuffle (array: any[]) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
 
 export {
   created,

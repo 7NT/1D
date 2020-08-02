70d01b137ffc727caeec9b944d849f82ca0ed17a
Jun Chen <jun@juns-mac-mini.attlocal.net>
2020-08-01 18:53:03 -0700

    8/1

--- src/build.bat
+++ src/build.bat
@@ -1,4 +1,4 @@
-icongenie generate -i public/jb2D.png
+icongenie generate -i public/jb1D.png
 quasar build -m capacitor -T android
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore jb1D.keystore dist/capacitor/android/apk/release/app-release-unsigned.apk alias_name
 zipalign -v 4 dist/capacitor/android/apk/release/app-release-unsigned.apk jb1d_beta.apk

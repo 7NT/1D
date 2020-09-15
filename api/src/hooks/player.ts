5bc477aa756ead9ca889dd0bcf5bf343e82d7777
Jun Chen <jun@juns-mac-mini.attlocal.net>
2020-09-15 12:59:54 -0700

    9/15

--- api/src/hooks/player.ts
+++ api/src/hooks/player.ts
@@ -1,7 +1,7 @@
 // Use this hook to manipulate incoming or outgoing data.
 // For more information on hooks see: http://docs.feathersjs.com/api/hooks.html
 import { Hook, HookContext } from '@feathersjs/feathers'
-import admin from 'firebase-admin'
+import * as admin from 'firebase-admin'
 
 // import mongoose from 'mongoose';
 import { jbIsPlayer } from '../jbPlayer'
